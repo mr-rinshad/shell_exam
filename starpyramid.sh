7ade403902764ef646f16255bfdc83351742035b
Muhammed Rinshad <mrmuhammedrinshad@gmail.com>
2026-04-27 19:48:23 +0530

    Add star pyramid script
    This script prompts the user for the number of rows and prints a star pyramid pattern based on the input.

---
+++ starpyramid.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "Enter rows: " n
+
+for ((i=1; i<=n; i++))
+do
+    for ((j=1; j<=n-i; j++))
+    do
+        echo -n " "
+    done
+
+    for ((k=1; k<=2*i-1; k++))
+    do
+        echo -n "*"
+    done
+
+    echo
+done
