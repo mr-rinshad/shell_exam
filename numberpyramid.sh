95b862e7bfaee7bf64a67ae401a389f08bf0f5fc
Muhammed Rinshad <mrmuhammedrinshad@gmail.com>
2026-04-27 19:46:22 +0530

    Add number pyramid script in numberpyramid.sh

---
+++ numberpyramid.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "Enter the number of rows: " n
+
+for ((i=1; i<=n; i++))
+do
+    for ((j=i; j<n; j++))
+    do
+        echo -n " "
+    done
+
+    for ((k=1; k<=i; k++))
+    do
+        echo -n "$k "
+    done
+
+    echo
+done
